0cce61515acd707ec83f06c39b4678fb5876a303
Zach Kohlberg <zach.kohlberg@gmail.com>
2024-03-22 17:06:50 -0400

    small update to compiled java notes

---
+++ notes/java/compiled_intro/code-blocks.java
@@ -0,0 +1,26 @@
+// braces on separate lines
+// more verbose, but extra lines visually separate body from surrounding code
+if (condition)
+{
+    body;
+}
+else if (condition)
+{
+    body;
+}
+else
+{
+    body;
+}
+
+// my preference (for Java code)
+// opening braces on same line as associated statement
+// else on same line as closing brace
+// shorter, but less whitespace to visually separate body from surrounding code
+if (condition) {
+    body;
+} else if (condition) {
+    body;
+} else {
+    body;
+}
---
+++ notes/java/compiled_intro/if-else.java
@@ -0,0 +1,14 @@
+// jshell won't like this
+if (condition) {
+    body;
+}
+else {
+    body;
+}
+
+// jshell will be fine with this
+if (condition) {
+    body;
+} else {
+    body;
+}
